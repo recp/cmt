4a664d8894637614fd3f08ba3744ddafdbee0b71
Recep Aslantas <info@recp.me>
2019-12-27 19:44:04 +0300

    resource enums

---
+++ include/cmt/resource.h
@@ -0,0 +1,39 @@
+/*
+ * Copyright (c), Recep Aslantas.
+ * MIT License (MIT), http://opensource.org/licenses/MIT
+ */
+
+#ifndef cmt_resource_h
+#define cmt_resource_h
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "common.h"
+
+typedef enum MtCPUCacheMode {
+  MtCPUCacheModeDefaultCache  = 0,
+  MtCPUCacheModeWriteCombined = 1
+} MtCPUCacheMode;
+
+typedef enum MtHazardTrackingMode {
+  MtHazardTrackingModeDefault   = 0,
+  MtHazardTrackingModeUntracked = 1,
+  MtHazardTrackingModeTracked   = 2
+} MtHazardTrackingMode;
+
+typedef enum MtResourceOptions {
+  MtResourceCPUCacheModeDefaultCache    = MtCPUCacheModeDefaultCache,
+  MtResourceCPUCacheModeWriteCombined   = MtCPUCacheModeWriteCombined,
+  
+  MtResourceStorageModeShared           = MtStorageModeShared           << 4,
+  MtResourceStorageModeManaged          = MtStorageModeManaged          << 4,
+  MtResourceStorageModePrivate          = MtStorageModePrivate          << 4,
+  MtResourceStorageModeMemoryless       = MtStorageModeMemoryless       << 4,
+  
+  MtResourceHazardTrackingModeDefault   = MtHazardTrackingModeDefault   << 8,
+  MtResourceHazardTrackingModeUntracked = MtHazardTrackingModeUntracked << 8,
+  MtResourceHazardTrackingModeTracked   = MtHazardTrackingModeTracked   << 8
+} MtResourceOptions;
+
+#endif /* cmt_resource_h */
