ba740534353213cbff909c6dbbbb75c25ee21c01
Recep Aslantas <info@recp.me>
2020-06-22 00:27:49 +0300

    mark api availability for heap descriptor

--- src/memory/heap-descriptor.m
+++ src/memory/heap-descriptor.m
@@ -12,72 +12,84 @@ MtHeapDescriptor*
 mtNewHeapDescriptor(void);
 
 MT_EXPORT
+MT_API_AVAILABLE(10.15, 13.0)
 MtHeapType 
 mtHeapDescriptorType(MtHeapDescriptor *heap) {
-	return [(MTLHeapDescriptor*)heap type];
+	return (MtHeapType)[(MTLHeapDescriptor*)heap type];
 }
 
 MT_EXPORT
+MT_API_AVAILABLE(10.15, 13.0)
 void
 mtHeapDescriptorTypeSet(MtHeapDescriptor *heap, MtHeapType type) {
 	[(MTLHeapDescriptor*)heap setType:(MTLHeapType)type];
 }
 
 MT_EXPORT
+MT_API_AVAILABLE(10.13, 10.0)
 MtStorageMode 
 mtHeapDescriptorStorageMode(MtHeapDescriptor *heap) {
-	return [(MTLHeapDescriptor*)heap storageMode];
+	return (MtStorageMode)[(MTLHeapDescriptor*)heap storageMode];
 }
 
 MT_EXPORT
+MT_API_AVAILABLE(10.13, 10.0)
 void
 mtHeapDescriptorStorageModeSet(MtHeapDescriptor *heap, MtStorageMode mode) {
 	[(MTLHeapDescriptor*)heap setStorageMode: (MTLStorageMode)mode];
 }
 
 MT_EXPORT
+MT_API_AVAILABLE(10.13, 10.0)
 MtCPUCacheMode 
 mtHeapDescriptorCPUCacheMode(MtHeapDescriptor *heap) {
-	return [(MTLHeapDescriptor*)heap cpuCacheMode];
+	return (MtCPUCacheMode)[(MTLHeapDescriptor*)heap cpuCacheMode];
 }
 
 MT_EXPORT
+MT_API_AVAILABLE(10.13, 10.0)
 void
 mtHeapDescriptorCpuCacheModeSet(MtHeapDescriptor *heap, MtCPUCacheMode mode) {
 	[(MTLHeapDescriptor*)heap setCpuCacheMode:(MTLCPUCacheMode)mode];
 }
 
 MT_EXPORT
+MT_API_AVAILABLE(10.15, 13.0)
 MtHazardTrackingMode 
 mtHeapDescriptorHazardTrackingMode(MtHeapDescriptor *heap) {
-	return [(MTLHeapDescriptor*)heap hazardTrackingMode];
+	return (MtHazardTrackingMode)[(MTLHeapDescriptor*)heap hazardTrackingMode];
 }
 
 MT_EXPORT
+MT_API_AVAILABLE(10.15, 13.0)
 void
 mtHeapDescriptorHazardTrackingModeSet(MtHeapDescriptor *heap, MtHazardTrackingMode mode) {
 	[(MTLHeapDescriptor*)heap setHazardTrackingMode: (MTLHazardTrackingMode)mode];
 }
 
 MT_EXPORT
+MT_API_AVAILABLE(10.15, 13.0)
 MtResourceOptions 
 mtHeapDescriptorResourceOptions(MtHeapDescriptor *heap) {
-	return [(MTLHeapDescriptor*)heap resourceOptions];
+	return (MtResourceOptions)[(MTLHeapDescriptor*)heap resourceOptions];
 }
 
 MT_EXPORT
+MT_API_AVAILABLE(10.15, 13.0)
 void
 mtHeapDescriptorResourceOptionsSet(MtHeapDescriptor *heap, MtResourceOptions opts) {
 	[(MTLHeapDescriptor*)heap setResourceOptions:(MTLResourceOptions)opts];
 }
 
 MT_EXPORT
+MT_API_AVAILABLE(10.13, 10.0)
 NsUInteger 
 mtHeapDescriptorSize(MtHeapDescriptor *heap) {
 	return [(MTLHeapDescriptor*)heap size];
 }
 
 MT_EXPORT
+MT_API_AVAILABLE(10.13, 10.0)
 void
 mtHeapDescriptorSizeSet(MtHeapDescriptor *heap, NsUInteger size) {
 	[(MTLHeapDescriptor*)heap setSize:size];
